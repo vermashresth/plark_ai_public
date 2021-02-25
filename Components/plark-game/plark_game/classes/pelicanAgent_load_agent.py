82499106dc21e1a4bc2da0ee42f8efaf8f500e90
nbarlowATI <nbarlow@turing.ac.uk>
2021-02-25 12:47:11 +0000

    add in_tournament flag to panther/pelicanAgent_loadAgent, and newgame.load_agent, and getTournamentAction to panther/pelicanAgent_loadAgent

--- Components/plark-game/plark_game/classes/pelicanAgent_load_agent.py
+++ Components/plark-game/plark_game/classes/pelicanAgent_load_agent.py
@@ -1,74 +1,96 @@
 import os
 from stable_baselines import DQN, PPO2, A2C, ACKTR
-from stable_baselines3 import PPO # PyTorch Stable Baselines 
+from stable_baselines3 import PPO  # PyTorch Stable Baselines
 from .pelicanAgent import Pelican_Agent
 import logging
-import numpy  as np
+import numpy as np
 from .pil_ui import PIL_UI
 import gc
 import torch
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class Pelican_Agent_Load_Agent(Pelican_Agent):
+    def __init__(
+        self,
+        filepath,
+        algorithm_type,
+        observation=None,
+        imaged_based=True,
+        in_tournament=False,
+    ):
 
-	def __init__(self, filepath, algorithm_type, observation = None, imaged_based=True):
+        if not in_tournament:
+            # Can't initialise this until we have game state
+            self.pil_ui = None
+            self.imaged_based = imaged_based
 
-		# Can't initialise this until we have game state
-		self.pil_ui = None
-		self.imaged_based = imaged_based
+            if not self.imaged_based:
+                if observation is not None:
+                    self.observation = observation
+                else:
+                    raise ValueError("Observation object not passed in to load agent.")
 
-		if not self.imaged_based: 
-			if observation is not None:
-				self.observation = observation
-			else:
-				raise ValueError('Observation object not passed in to load agent.' )
+            # load the agent
+            if os.path.exists(filepath):
+                self.loadAgent(filepath, algorithm_type)
+                logger.info("pelican agent loaded")
+            else:
+                raise ValueError(
+                    'Error loading pelican agent. File : "'
+                    + filepath
+                    + '" does not exsist'
+                )
 
-		if os.path.exists(filepath): 
-			self.loadAgent(filepath , algorithm_type)
-			logger.info('pelican agent loaded')
-		else:
-			raise ValueError('Error loading pelican agent. File : "' + filepath + '" does not exsist' )
-        
-	def loadAgent(self, filepath, algorithm_type ):
-		try:
-			if algorithm_type.lower() == 'dqn':
-				self.model = DQN.load(filepath)
-			elif algorithm_type.lower() == 'ppo2':
-				self.model = PPO2.load(filepath)
-			elif algorithm_type.lower() == 'ppo':
-				self.model = PPO.load(filepath)
-			elif algorithm_type.lower() == 'a2c':
-				self.model = A2C.load(filepath)
-			elif algorithm_type.lower() == 'acktr':
-				self.model = ACKTR.load(filepath)
-		except:
-			raise ValueError('Error loading pelican agent. File : "' + filepath + '" does not exsist' )
+    def loadAgent(self, filepath, algorithm_type):
+        try:
+            if algorithm_type.lower() == "dqn":
+                self.model = DQN.load(filepath)
+            elif algorithm_type.lower() == "ppo2":
+                self.model = PPO2.load(filepath)
+            elif algorithm_type.lower() == "ppo":
+                self.model = PPO.load(filepath)
+            elif algorithm_type.lower() == "a2c":
+                self.model = A2C.load(filepath)
+            elif algorithm_type.lower() == "acktr":
+                self.model = ACKTR.load(filepath)
+        except:
+            raise ValueError(
+                'Error loading pelican agent. File : "' + filepath + '" does not exsist'
+            )
 
-		
-	def getAction(self, state):
-		# Eventually this should be replaced by a helper method that doesn't require constructing a class instance
-		if not self.pil_ui:
-			self.pil_ui = PIL_UI(
-				state,
-				state['hexScale'],
-				state['view_all'],
-				state['sb_display_range'],
-				state['render_width'],
-				state['render_height'],
-				state['sb_range'],
-				state['torpedo_hunt_enabled'],
-				state['torpedo_speeds']
-			)
+    def getAction(self, state):
+        # Eventually this should be replaced by a helper method that doesn't require constructing a class instance
+        if not self.pil_ui:
+            self.pil_ui = PIL_UI(
+                state,
+                state["hexScale"],
+                state["view_all"],
+                state["sb_display_range"],
+                state["render_width"],
+                state["render_height"],
+                state["sb_range"],
+                state["torpedo_hunt_enabled"],
+                state["torpedo_speeds"],
+            )
 
-		if self.imaged_based:
-			obs = self.pil_ui.update(state)
-			obs = np.array(obs, dtype=np.uint8)
-		else:
-			obs = self.observation.get_observation(state) 
-			
-		action, _ = self.model.predict(obs, deterministic=False)
-		return self.action_lookup(action)
+        if self.imaged_based:
+            obs = self.pil_ui.update(state)
+            obs = np.array(obs, dtype=np.uint8)
+        else:
+            obs = self.observation.get_observation(state)
 
+        action, _ = self.model.predict(obs, deterministic=False)
+        return self.action_lookup(action)
 
-
+        def getTournamentAction(self, obs, obs_normalised, domain_parameters, state):
+            """
+            PARTICIPANTS:  the example below calls model.predict on a stable_baselines
+            agent that expects the observation (a list of numbers).
+            Modify this if you have an agent that expects a different input, for
+            example obs_normalised, or state, or if it needs the domain_parameters.
+            """
+            action, _ = self.model.predict(obs, deterministic=False)
+            return self.action_lookup(action)
