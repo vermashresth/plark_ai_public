bf0a59f3cfe229b16c34ba32926c8f6c0b93972e
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2021-03-02 10:53:34 +0200

    adding test files

--- Combatant/tests/test_combatant.py
+++ Combatant/tests/test_combatant.py
@@ -5,6 +5,7 @@ import sys
 import json
 from combatant import load_combatant
 from schema import deserialize_state
+import numpy as np
 
 ALLOWED_ACTIONS = {
     "PANTHER": [
@@ -31,8 +32,6 @@ ALLOWED_ACTIONS = {
 
 AGENT_NAME = ""
 
-TEST_PATH = os.path.join("/plark_ai_public", "Combatant", "tests", "states")
-
 agent_type = sys.argv[1]
 
 basic_agents_path = os.path.join(
@@ -45,37 +44,48 @@ basic_agents_path = os.path.join(
 )
 
 agent_path = os.path.join(
-    "/plark_ai_public", "data", "agents", "models", "latest"
+    "/plark_ai_public", "data", "agents", "models", "move_north_nn"
 )
 
-
 if agent_type == "PELICAN":
-    subdirs = os.listdir(os.path.join(agent_path, "pelican"))
-    for subdir in subdirs:
-        agent_path = os.path.join(agent_path, "pelican", subdir)
-        break
+    # subdirs = os.listdir(os.path.join(agent_path, "pelican"))
+    # for subdir in subdirs:
+    #     agent_path = os.path.join(agent_path, "pelican", subdir)
+    #     break
 
-    state = deserialize_state(
-        json.load(open(os.path.join(TEST_PATH, "state_10x10_pelican.json")))
-    )
+    test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "pelican")
+    
 elif agent_type == "PANTHER":
 
-    subdirs = os.listdir(os.path.join(agent_path, "panther"))
-    for subdir in subdirs:
-        agent_path = os.path.join(agent_path, "panther", subdir)
-        break
+    # subdirs = os.listdir(os.path.join(agent_path, "panther"))
+    # for subdir in subdirs:
+    #     agent_path = os.path.join(agent_path, "panther", subdir)
+    #     break
+    
+    test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "panther")
 
-    state = deserialize_state(
-        json.load(open(os.path.join(TEST_PATH, "state_10x10_panther.json")))
-    )
 else:
     raise RuntimeError("Unknown agent_type - must be 'PELICAN' or 'PANTHER'")
 
-print("agent_path: ", agent_path)
+state = deserialize_state(
+    json.load(open(os.path.join(test_path, "10x10_state.json")))
+)
+
+obs = np.loadtxt(os.path.join(test_path, "10x10_obs.txt"))
+obs_norm = np.loadtxt(os.path.join(test_path, "10x10_obs_norm.txt"))
+d_params = np.loadtxt(os.path.join(test_path, "10x10_domain_params.txt"))
+d_params_norm = np.loadtxt(os.path.join(test_path, "10x10_domain_params_norm.txt"))
+
 
 agent = load_combatant(agent_path, AGENT_NAME, basic_agents_path)
 
-action = agent.getAction(state)
+action = agent.getTournamentAction(
+    obs,
+    obs_norm,
+    d_params,
+    d_params_norm,
+    state
+)
 
 if action not in ALLOWED_ACTIONS[agent_type]:
     raise RuntimeError("NO!")
