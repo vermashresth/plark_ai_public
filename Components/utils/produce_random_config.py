f61eb5070df7ed494696d0e40f022f0a9f871f02
James Butterworth <jjbsports16@hotmail.co.uk>
2021-03-04 09:25:13 +0000

    Fix produce random config script bug

--- Components/utils/produce_random_config.py
+++ Components/utils/produce_random_config.py
@@ -4,5 +4,5 @@ from domain_parameters_json import *
 rand_params = generate_random_param_instance()
 print(rand_params)
 
-dump_param_instance_json(rand_params)
+dump_param_instance_json(rand_params, 'random_instance.json')
 
